850b2451634af0b65b491656a4816d8538087227
Abhijith Tammanagari <23abhijith@gmail.com>
2021-11-12 19:17:06 -0500

    Add files via upload

---
+++ weather-cli.py
@@ -0,0 +1,43 @@
+import click
+
+from flask import jsonify
+import requests
+import geocoder
+import json
+
+
+@click.command()
+def weatherForMe():
+    currentLocation = geocoder.ip('me').latlng
+    response = json.loads(requests.get('https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly=temperature_2m&temperature_unit=fahrenheit'.format(currentLocation[0], currentLocation[1])).text)
+    times = response.get('hourly').get('time')
+    temps = response.get('hourly').get('temperature_2m')
+    g = geocoder.osm([currentLocation[0], currentLocation[1]], method='reverse')
+    tempsTime = {}
+    tempsTime['address'] = g.json['address']
+    weather = {}
+    for idx in range(len(times)):
+        weather[times[idx]] = temps[idx]
+    tempsTime['weather'] = weather
+    return click.echo(tempsTime)
+
+@click.command()
+@click.option('--location', nargs=2, type=float)
+def weatherForLocation(location):
+    a, b = location
+    response = json.loads(requests.get('https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly=temperature_2m&temperature_unit=fahrenheit'.format(a, b)).text)
+    times = response.get('hourly').get('time')
+    temps = response.get('hourly').get('temperature_2m')
+    g = geocoder.osm([a, b], method='reverse')
+    tempsTime = {}
+    tempsTime['address'] = g.json['address']
+    weather = {}
+    for idx in range(len(times)):
+        weather[times[idx]] = temps[idx]
+    tempsTime['weather'] = weather
+    return click.echo(tempsTime)
+
+
+if __name__ == '__main__':
+    weatherForLocation()
+    weatherForMe()
